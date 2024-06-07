5eff7b8d722c57fc9a375e7d1a59051664bdf424
nguyen_thanh_quy@allgrow-labo.jp <nguyen_thanh_quy@allgrow-labo.jp>
2024-06-07 17:10:55 +0700

    up add to delete todo lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const MainButton = ({ active = "", children }) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button type="button" className={"px-4 py-1 rounded-md " + active}> {children}</button >
   )
 }
 
-export default App
+let nextId = 0
+
+export default function App() {
+
+  const [lists, setLists] = useState([])
+
+  const [todo, setTodo] = useState('')
+
+  function handleTodoChange(e) {
+    setTodo(e.target.value)
+  }
+
+  function handleListsChange() {
+    if (event.key === 'Enter') {
+      if (todo != '') {
+        setLists([
+          ...lists,
+          { id: nextId++, todo: todo }
+        ])
+        setTodo('')
+      }
+    }
+  }
+
+  return (
+    <div className="min-h-screen bg-slate-300 flex flex-col pt-20 items-center gap-5">
+      <h1 className="text-5xl font-bold">📓 Todo List</h1>
+      <div className="flex flex-col bg-white min-w-[600px] min-h-[432px] rounded py-4 px-5 shadow-md">
+        <div className="flex">
+          <input type="text" onChange={handleTodoChange} onKeyDown={handleListsChange} value={todo} className='border-b w-full p-2 mb-1 focus:outline-none' placeholder='Viết todo của bạn ở đây....' />
+        </div>
+
+        {
+          lists.map((item) => (
+            <div className="group border-b py-3 px-2 mb-1 relative" key={item.id}>
+              <label className="peer flex gap-2 items-center">
+                <input className="peer size-3.5 appearance-none rounded-sm border border-slate-300 accent-pink-500 checked:appearance-auto" type="checkbox" name="1" />
+                <span className="peer-checked:text-slate-400 peer-checked:line-through">
+                  {item.todo}
+                </span>
+                <button className="bg-green-600 text-white px-2 py-1 text-sm rounded">edit</button>
+              </label>
+              <button className="group-hover:opacity-100 opacity-0 transition-all absolute top-1/2 right-0 -translate-y-1/2 text-red-600 p-2" onClick={() => { setLists(lists.filter(a => a.id !== item.id)) }}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+          ))
+        }
+
+        <div className="flex justify-between items-center text-sm text-gray-800 mt-auto">
+          <div>
+            <span>{lists.length} item(s) left</span>
+          </div>
+          <div className="flex gap-1">
+            <MainButton active="border">all</MainButton>
+            <MainButton>active</MainButton>
+            <MainButton>completed</MainButton>
+          </div>
+          <div>
+            <MainButton>Clear Completed</MainButton>
+          </div>
+        </div>
+
+      </div>
+      <p className="text-center text-sm text-gray-500 font-mono">© AGL</p>
+    </div>
+  )
+}
